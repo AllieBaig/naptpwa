8cfc59dad2677828241f4fcc646d961fea6d93c8
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-22 14:04:34 +0100

    Create version.js

---
+++ scripts/utils/version.js
@@ -0,0 +1,25 @@
+// MIT License
+// Copyright (c) 2025 AllieBaig
+// https://github.com/AllieBaig/naptpwa/blob/main/LICENSE
+
+// Version map for all major modules
+export const versionMap = {
+  // Core Scripts
+  gameNavigation: 'v1.2.0',
+  settings: 'v1.1.0',
+  errorHandler: 'v1.1.1',
+  autosave: 'v1.0.1',
+  voice: 'v1.0.0',
+  clues: 'v1.0.0',
+  fontControls: 'v1.0.2',
+  streak: 'v1.1.0',
+  history: 'v1.1.0',
+
+  // Game Modes
+  regular: 'v1.2.0',
+  wordRelic: 'v1.2.0',
+  wordSafari: 'v1.2.0',
+  dice: 'v1.1.1',
+  atlas: 'v1.0.5',
+  versus: 'v1.0.2'
+};
